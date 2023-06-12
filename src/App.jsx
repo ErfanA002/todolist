1802d3164a38e022905c2226feacfda67da54d0a
ErfanA002 <120725773+ErfanA002@users.noreply.github.com>
2023-06-12 13:41:46 +0330

    add files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-black">
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        quidem repellendus cumque sit omnis ea, consequatur ad illo dignissimos,
+        aspernatur vero, corrupti consequuntur voluptatibus voluptates laborum
+        saepe optio aliquam unde!
+      </p>
+    </div>
+  );
+}
+
+export default App;
